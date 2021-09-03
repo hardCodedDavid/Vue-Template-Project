a77eead9109113f00ab6fe09078464878fa8c1a8
101David <88326987+101David@users.noreply.github.com>
2021-09-03 12:14:56 +0100

    Add files via upload

---
+++ storage/framework/views/0b9a35fa7a1ad0659cdf83e96fd0d88b6ec1bfcf.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        
+    </head>
+    <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            <?php if(Route::has('login')): ?>
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    <?php if(auth()->guard()->check()): ?>
+                        <a href="<?php echo e(url('/home')); ?>" class="text-sm text-gray-700 underline">Home</a>
+                    <?php else: ?>
+                        <a href="<?php echo e(route('login')); ?>" class="text-sm text-gray-700 underline">Log in</a>
+
+                        <?php if(Route::has('register')): ?>
+                            <a href="<?php echo e(route('register')); ?>" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+
+    <div id="app">
+            <example-component>
+            </example-component>
+        </div>
+        
+        </div>
+    </body>
+</html>
+<?php /**PATH C:\Users\HP\Documents\Laravel and Vue\vue-app\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/28cb61902aa968789a1178725d708ac577f44680.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+    <title>Data Template</title>
+
+    <!-- Scripts -->
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+</head>
+<body>
+	<div id="app">
+		<example-component></example-component>
+	</div>
+</body>
+</html>
+
+<?php /**PATH C:\Users\HP\Documents\Laravel and Vue\vue-app\resources\views/data.blade.php ENDPATH**/ ?>
